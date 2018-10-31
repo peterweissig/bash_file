f1478dbcb16d94e5711c6ad660526b5f15d38130
Peter Weissig <peterweissig@github.com>
2018-10-31 10:38:07 +0100

    updated backup-functions and help-function   + created backup_base from backup_simple (2x ToDos)   + added inplace_backup   + updated help-functions

--- scripts/backup_functions.sh
+++ scripts/backup_functions.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
 #***************************[backup]******************************************
-# 2018 10 29
+# 2018 10 30
 
-function file_backup_simple() {
+function _file_backup_base() {
 
     # print help
     if [ "$1" == "-h" ]; then
-        echo "$FUNCNAME <filename>"
+        echo "$FUNCNAME <filename> <backup_path> <name_style>"
 
         return
     fi
     if [ "$1" == "--help" ]; then
-        echo "$FUNCNAME needs 1 parameter"
-        echo "     #1: name of file to be backed up (e.g. \"table.odt\")"
+        echo "$FUNCNAME needs 3 parameters"
+        echo "     #1: name/path of file to be backed up"
+        echo "         (e.g. \"~/table.odt\" or \"/etc/fstab\")"
+        echo "     #2: path of backup-folder (e.g. \"~/backup/\")"
+        echo "         this string needs to end in \"/\""
+        echo "     #3: naming convention - either prefix or suffix filename"
+        echo   "         with current date (and consecutive number)"
+        echo "         \"prefix\" --> ~/backup/2018_10_30[_004]__table.odt"
+        echo "         \"suffix\" --> ~/backup/fstab__2017_11_01[_002]"
         echo "This function will copy the given file to the backup-folder"
-        echo "and prepend it with the current date."
-        echo "  (e.g. save \"table.odt\" to \"backup/2018_10_30__table.odt\")"
-        echo "If neither of the two possible local backup-folders"
-        echo "(backup nor data) exist, \"backup/\" will be created."
+        echo "and prepend or extend it with the current date."
 
         return
     fi
 
     # check parameter
-    if [ $# -ne 1 ]; then
+    if [ $# -ne 3 ]; then
         echo "$FUNCNAME: Parameter Error."
         $FUNCNAME --help
         return -1
     fi
+
+    name_style="$3"
+    if [ "$name_style" != "prefix" ] && [ "$name_style" != "suffix" ]; then
+        echo "$FUNCNAME: Naming style must be prefix or suffix."
+        echo "  (not \"$name_style\")"
+        return -1
+    fi
+
+    # TODO
+    echo "$FUNCNAME: TODO - remove old prefix/suffix date!"
 
     # init variables
     filename="$1"
@@ -40,6 +54,8 @@ function file_backup_simple() {
         filepath="${filepath}/"
     fi
 
+    backup_path="${2}"
+
     # check if file exists
     if [ ! -f "$filename" ]; then
         echo "$FUNCNAME: File \"$filename\" not found."
@@ -47,34 +63,38 @@ function file_backup_simple() {
     fi
 
     # check backup path (eventually create it)
-    backuppath="${filepath}backup/"
-    if [ ! -d "$backuppath" ]; then
-        backuppath2="${filepath}data/"
-        if [ -d "$backuppath2" ]; then
-            echo "using \"$backuppath\" as backup-folder"
-            backuppath="$backuppath2"
-        else
-            echo "creating backup-folder \"$backuppath\""
-            mkdir -p "$backuppath"
-            if [ $? -ne 0 ]; then
-                echo "$FUNCNAME: Stopping because of an error."
-                return -1;
-            fi
+    if [ "$backup_path" != "" ] && [ ! -d "$backup_path" ]; then
+        echo "creating backup-folder \"$backup_path\""
+        mkdir -p "$backup_path"
+        if [ $? -ne 0 ]; then
+            echo "$FUNCNAME: Stopping because of an error."
+            return -1;
         fi
     fi
 
-    # get current prefix
-    fileprefix="$(date +"%Y_%m_%d_")"
+    # get current extension (prefix or suffix)
+    file_date="$(date +"%Y_%m_%d")"
 
     # check for older backups (simple)
     found=0
-    backup_simple="${backuppath}${fileprefix}_${filebase}"
+    if [ "$name_style" == "prefix" ]; then
+        backup_simple="${backup_path}${file_date}__${filebase}"
+    else
+        backup_simple="${backup_path}${filebase}__${file_date}"
+    fi
     if [ -f "${backup_simple}" ]; then
         found=1
-        temp="${backuppath}${fileprefix}001__${filebase}"
 
-        echo "found other backup \"${backup_simple}\""
-        echo "it is going to be renamed to \"${temp}\""
+        # create new name for older backup
+        if [ "$name_style" == "prefix" ]; then
+            temp="${backup_path}${file_date}_001__${filebase}"
+        else
+            temp="${backup_path}${filebase}__${file_date}_001"
+        fi
+
+        # inform user
+        echo "found other backup"
+        echo "  \"${backup_simple}\" ==> \"${temp}\""
         if [ -f "${temp}" ]; then
             echo "$FUNCNAME: Error - \"${temp}\" already exists!"
             return -1;
@@ -100,10 +120,15 @@ function file_backup_simple() {
     else
 
         # check for older backups (with numbering)
-        temp="${backuppath}${fileprefix}[0-9][0-9][0-9]__${filebase}"
-        arr=("${backuppath}${fileprefix}"[0-9][0-9][0-9]"__${filebase}")
-        echo "arr= \"${arr[*]}\""
+        if [ "$name_style" == "prefix" ]; then
+            temp="${backup_path}${file_date}_[0-9][0-9][0-9]__${filebase}"
+            arr=("${backup_path}${file_date}_"[0-9][0-9][0-9]"__${filebase}")
+        else
+            temp="${backup_path}${filebase}__${file_date}_[0-9][0-9][0-9]"
+            arr=("${backup_path}${filebase}__${file_date}_"[0-9][0-9][0-9])
+        fi
         if [ "${#arr[*]}" -gt 1 ] || [ "${arr[0]}" != "${temp}" ]; then
+            echo "found other backup(s) - extending backup name"
             found=1
         fi
     fi
@@ -118,10 +143,14 @@ function file_backup_simple() {
     # otherwise find a backup name
     cnt="1"
     while [ "$cnt" -lt 1000 ]; do
-        temp="${fileprefix}$(printf "%03d" $cnt)__${filebase}"
-        backup_nr="${backuppath}${temp}"
+        if [ "$name_style" == "prefix" ]; then
+            temp="${file_date}_$(printf "%03d" $cnt)__${filebase}"
+        else
+            temp="${filebase}__${file_date}_$(printf "%03d" $cnt)"
+        fi
+        backup_nr="${backup_path}${temp}"
 
-        if [ ! -f "${backup_nr}" ]; then
+        if [ ! -e "${backup_nr}" ]; then
             echo "creating backup \"${backup_nr}\""
             cp "${filename}" "${backup_nr}"
             return
@@ -132,4 +161,106 @@ function file_backup_simple() {
 
     echo "$FUNCNAME: Can not create valid backup file :-("
     return -3;
+}
+
+function file_backup_simple() {
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME <filename>"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 1 parameter"
+        echo "     #1: name of file to be backed up (e.g. \"table.odt\")"
+        echo "This function will copy the given file to the backup-folder"
+        echo "and prepend it with the current date."
+        echo "  (e.g. save \"table.odt\" to \"backup/2018_10_30__table.odt\")"
+        echo "If none of the two possible local backup-folders"
+        echo "(neither backup nor data) exist, \"backup/\" will be created."
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -ne 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # TODO
+    echo "$FUNCNAME: TODO - check if already in backup-folder!"
+    echo "    (not data folder)"
+
+    # init variables
+    filename="$1"
+    filepath="$(dirname  "$filename")"
+    if [ "$filepath" == "." ]; then
+        filepath=""
+    else
+        filepath="${filepath}/"
+    fi
+
+    # check if file exists
+    if [ ! -f "$filename" ]; then
+        echo "$FUNCNAME: File \"$filename\" not found."
+        return -2
+    fi
+
+    # check for possible backup path
+    backup_path="${filepath}backup/"
+    if [ ! -d "$backup_path" ]; then
+        backup_path2="${filepath}data/"
+        if [ -d "$backup_path2" ]; then
+            echo "using \"$backup_path2\" as backup-folder"
+            backup_path="$backup_path2"
+        fi
+    fi
+
+    _file_backup_base "$filename" "$backup_path" "prefix"
+}
+
+function file_backup_inplace() {
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME <filename>"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 1 parameter"
+        echo "     #1: name of file to be backed up (e.g. \"file.conf\")"
+        echo "This function will create a copy the given file within the"
+        echo "same folder and extend it with the current date."
+        echo "  (e.g. save \"file.conf\" to \"file.conf_2018_10_30\")"
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -ne 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # init variables
+    filename="$1"
+    filepath="$(dirname  "$filename")"
+    if [ "$filepath" == "." ]; then
+        filepath=""
+    else
+        filepath="${filepath}/"
+    fi
+
+    # check if file exists
+    if [ ! -f "$filename" ]; then
+        echo "$FUNCNAME: File \"$filename\" not found."
+        return -2
+    fi
+
+    _file_backup_base "$filename" "$filepath" "suffix"
 }
--- scripts/help.sh
+++ scripts/help.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-#***************************[help]********************************************
-# 2018 10 29
+#***************************[all]*********************************************
+# 2018 10 31
 
-function file_help() {
+function file_help_all() {
 
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 0 parameters"
+        echo "Prints all available functions within repository \"file\"."
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -gt 0 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    # print overview of all repositories
     echo ""
     echo "### $FUNCNAME ###"
+    echo ""
+    echo "help"
+    echo -n "  "; echo "file_help"
+    echo -n "  "; file_help_all -h
     echo ""
     echo "filenames"
     echo -n "  "; file_name_clean -h
@@ -15,5 +41,29 @@ function file_help() {
     echo ""
     echo "backup"
     echo -n "  "; file_backup_simple -h
+    echo -n "  "; file_backup_inplace -h
+    echo -n "  "; _file_backup_base -h
+    echo ""
+}
+
+#***************************[help]********************************************
+# 2018 10 31
+
+function file_help() {
+
+    echo ""
+    echo "### $FUNCNAME ###"
+    echo ""
+    echo "help functions"
+    echo -n "  "; echo "file_help"
+    echo -n "  "; file_help_all -h
+    echo ""
+    echo "filenames"
+    echo -n "  "; file_name_clean -h
+    echo -n "  "; file_name_expand -h
+    echo ""
+    echo "backup"
+    echo -n "  "; file_backup_simple -h
+    echo -n "  "; file_backup_inplace -h
     echo ""
 }

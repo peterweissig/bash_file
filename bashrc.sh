a6038bf537cfacadf7cc2196def374aedabc53a1
Peter Weissig <peterweissig@github.com>
2021-03-24 22:08:15 +0100

    fixed sourcing   + escaping all paths   + using "source" instead of "."

--- bashrc.sh
+++ bashrc.sh
@@ -18,16 +18,18 @@ fi
 export SOURCED_BASH_FILE="$SOURCED_BASH_LAST"
 
 
+
 #***************************[paths and files]*********************************
 # 2020 12 27
 
 temp_local_path="$(realpath "$(dirname "${BASH_SOURCE}")" )/"
 
 
-#***************************[source]******************************************
-# 2021 01 07
 
-. ${temp_local_path}scripts/filename_functions.sh
-. ${temp_local_path}scripts/backup_functions.sh
-. ${temp_local_path}scripts/search_functions.sh
-. ${temp_local_path}scripts/help.sh
+#***************************[source]******************************************
+# 2021 03 24
+
+source "${temp_local_path}scripts/filename_functions.sh"
+source "${temp_local_path}scripts/backup_functions.sh"
+source "${temp_local_path}scripts/search_functions.sh"
+source "${temp_local_path}scripts/help.sh"
